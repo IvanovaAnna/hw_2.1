3d49c201cf3ff99ac5312a03d7e27b5056f56ca9
Ivanova Anna <annprokudina@gmail.com>
2021-10-23 14:15:24 +0300

    added rgb logic

--- hw_2.1/ViewController.swift
+++ hw_2.1/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var intensityRed: UILabel!
+    @IBOutlet var intensityGreen: UILabel!
+    @IBOutlet var intensityBlue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+    }
+    
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
 
-
+    @IBAction func rgbAction(_ sender: Any) {
+        changeColor()
+    }
+    
 }
 
